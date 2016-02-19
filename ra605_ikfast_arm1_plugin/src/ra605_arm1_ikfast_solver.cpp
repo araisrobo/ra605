0ce2e1cd8f957f0035b0bf74aa3060e623070b10
Yishin Li <ysli@araisrobo.com>
2016-02-19 13:30:05 +0800

    Update RA605.URDF

--- ra605_ikfast_arm1_plugin/src/ra605_arm1_ikfast_solver.cpp
+++ ra605_ikfast_arm1_plugin/src/ra605_arm1_ikfast_solver.cpp
@@ -12,7 +12,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
-/// ikfast version 61 generated on 2016-02-19 09:57:41.269011
+/// ikfast version 61 generated on 2016-02-19 13:13:13.682465
 /// To compile with gcc:
 ///     gcc -lstdc++ ik.cpp
 /// To compile without any main function as a shared object (might need -llapack):
@@ -2498,7 +2498,7 @@ IKSolver solver;
 return solver.ComputeIk(eetrans,eerot,pfree,solutions);
 }
 
-IKFAST_API const char* GetKinematicsHash() { return "<robot:genericrobot - ra605 (5cc4b591de2c9071abaf34adfaf0ca67)>"; }
+IKFAST_API const char* GetKinematicsHash() { return "<robot:genericrobot - ra605 (25b0e0c666b434e9b53c37ff7e8200ff)>"; }
 
 IKFAST_API const char* GetIkFastVersion() { return IKFAST_STRINGIZE(IKFAST_VERSION); }
 
--- ra605_moveit_config/scripts/moveit_ik_demo.py
+++ ra605_moveit_config/scripts/moveit_ik_demo.py
@@ -34,51 +34,61 @@ class MoveItDemo:
         # Initialize the move_group API
         moveit_commander.roscpp_initialize(sys.argv)
         
+        # sim = False; # for RPi2
+        sim = True;
+
         rospy.init_node('moveit_demo')
                 
         # Initialize the move group for the right arm
         arm1 = moveit_commander.MoveGroupCommander('arm1')
                 
         # Get the name of the end-effector link
+        # end_effector_link = arm1.get_end_effector_link()
+        arm1.set_end_effector_link('link6')
         end_effector_link = arm1.get_end_effector_link()
                         
         # Set the reference frame for pose targets
         reference_frame = 'base_link'
+        # reference_frame = 'link5'
         
         # Set the right arm reference frame accordingly
         arm1.set_pose_reference_frame(reference_frame)
                 
         # Allow replanning to increase the odds of a solution
-        arm1.allow_replanning(True)
+        arm1.allow_replanning(False)
         
         # Allow some leeway in position (meters) and orientation (radians)
         arm1.set_goal_position_tolerance(0.01)
         arm1.set_goal_orientation_tolerance(0.05)
         
-        # Start the arm in the "init" pose stored in the SRDF file
-        arm1.set_named_target('init')
-        arm1.go()
-        rospy.sleep(2)
+#        # Start the arm in the "init" pose stored in the SRDF file
+#        arm1.set_named_target('init')
+#        arm1.go()
+#        if sim:
+#            rospy.sleep(2)
                
-        # Set the target pose.  This particular pose has the gripper oriented horizontally
-        # 0.85 meters above the ground, 0.10 meters to the right and 0.20 meters ahead of 
-        # the center of the robot base.
-        target_pose = PoseStamped()
-        target_pose.header.frame_id = reference_frame
-        target_pose.header.stamp = rospy.Time.now()     
-        target_pose.pose.position.x = 0.20
-        target_pose.pose.position.y = -0.1
-        target_pose.pose.position.z = 0.85
-        target_pose.pose.orientation.x = 0.0
-        target_pose.pose.orientation.y = 0.0
-        target_pose.pose.orientation.z = 0.0
-        target_pose.pose.orientation.w = 1.0
+#         # Set the target pose.  This particular pose has the gripper oriented horizontally
+#         # 0.85 meters above the ground, 0.10 meters to the right and 0.20 meters ahead of 
+#         # the center of the robot base.
+#         target_pose = PoseStamped()
+#         target_pose.header.frame_id = reference_frame
+#         target_pose.header.stamp = rospy.Time.now()     
+#         target_pose.pose.position.x = 0.20
+#         target_pose.pose.position.y = 0.3
+#         target_pose.pose.position.z = 0.55
+#         target_pose.pose.orientation.x = 0.0
+#         target_pose.pose.orientation.y = 0.0
+#         target_pose.pose.orientation.z = 0.0
+#         target_pose.pose.orientation.w = 1.0
         
         # Set the start state to the current state
         arm1.set_start_state_to_current_state()
         
         # Set the goal pose of the end effector to the stored pose
-        arm1.set_pose_target(target_pose, end_effector_link)
+#        arm1.set_pose_target(target_pose, end_effector_link)
+
+        # Shift the end-effector to the right 5cm
+        arm1.shift_pose_target(2, -0.05, end_effector_link)  # roll
         
         # Plan the trajectory to the goal
         traj = arm1.plan()
@@ -86,35 +96,48 @@ class MoveItDemo:
         # Execute the planned trajectory
         arm1.execute(traj)
     
-        # Pause for a second
-        rospy.sleep(1)
-         
+        if sim:
+            rospy.sleep(2)
+        
+        # Shift (3/Roll) the end-effector to the left 5cm
+        arm1.shift_pose_target(2, 0.05, end_effector_link)
+        
+        # Plan the trajectory to the goal
+        traj = arm1.plan()
+        
+        # Execute the planned trajectory
+        arm1.execute(traj)
+    
+        if sim:
+            rospy.sleep(2)
+        
         # Shift the end-effector to the right 5cm
         arm1.shift_pose_target(1, -0.05, end_effector_link)
         arm1.go()
-        rospy.sleep(1)
+        if sim:
+            rospy.sleep(2)
   
-        # Rotate the end-effector 90 degrees
-        arm1.shift_pose_target(3, -1.57, end_effector_link)
-        arm1.go()
-        rospy.sleep(1)
-          
-        # Store this pose as the new target_pose
-        saved_target_pose = arm1.get_current_pose(end_effector_link)
-          
-        # Move to the named pose "vertical"
-        arm1.set_named_target('vertical')
-        arm1.go()
-        rospy.sleep(1)
-          
-        # Go back to the stored target
-        arm1.set_pose_target(saved_target_pose, end_effector_link)
-        arm1.go()
-        rospy.sleep(1)
+#         # Rotate the end-effector 90 degrees
+#         arm1.shift_pose_target(3, -1.57, end_effector_link)
+#         arm1.go()
+#         rospy.sleep(1)
+#           
+#         # Store this pose as the new target_pose
+#         saved_target_pose = arm1.get_current_pose(end_effector_link)
+#           
+#         # Move to the named pose "vertical"
+#         arm1.set_named_target('vertical')
+#         arm1.go()
+#         rospy.sleep(1)
+#           
+#         # Go back to the stored target
+#         arm1.set_pose_target(saved_target_pose, end_effector_link)
+#         arm1.go()
+#         rospy.sleep(1)
            
         # Finish up in the init position  
-        arm1.set_named_target('init')
-        arm1.go()
+#        arm1.set_named_target('init')
+#        arm1.go()
 
         # Shut down MoveIt cleanly
         moveit_commander.roscpp_shutdown()
